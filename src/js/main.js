fd790b46a2abfc4635b6f5ec8c43069d1363157a
Dendenzilla <martinadriaensen@gmail.com>
2021-07-16 00:30:12 +0200

    various changes in css, js and php

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 // IF COMING FROM HOME OR ON HOME
-if ((document.referrer === 'http://localhost:3000/index.html') || (document.referrer === 'http://localhost:3000/')  ||( window.location.href === 'http://localhost:3000/index.html' ) || ( window.location.href === 'http://localhost:3000/' )) {
+if ((document.referrer === 'https://dekube.be/') || (document.referrer === 'http://dekube.be/') || (document.referrer === 'http://localhost:3000/')  ||( window.location.href === 'https://dekube.be/' ) || ( window.location.href === 'http://dekube.be/' ) || ( window.location.href === 'http://localhost:3000/' )) {
     $( document ).ready(function() {
         $('.page-loader-base.bottom-left').removeClass('loading');
         console.log("either on home or coming from home");
@@ -8,7 +8,7 @@ if ((document.referrer === 'http://localhost:3000/index.html') || (document.refe
         $('.page-loader-base.bottom-left').addClass('loaded');
         console.log("not on home or coming from home");
 }
-if(( window.location.href !== 'http://localhost:3000/index.html' ) && ( window.location.href !== 'http://localhost:3000/' )){
+if(( window.location.href !== 'https://dekube.be/' ) && ( window.location.href !== 'http://dekube.be/' ) && ( window.location.href !== 'http://localhost:3000/' )){
     setTimeout(function(){ 
         var nextHeader= $('li.current-menu-item').text();
         $('.link-home span.submenu').text(nextHeader);
@@ -25,12 +25,41 @@ if(( window.location.href !== 'http://localhost:3000/index.html' ) && ( window.l
 }
 // Closing the main page loader on page load and opening it imemdiatly
 // Taken out of the "doc.ready" fct so there's no blink during which user can see the main area
-if ((document.referrer !== 'http://localhost:3000/index.html') || (document.referrer === 'http://localhost:3000/') && (sessionStorage.getItem("isOpen") == "True")){
+// first if so that when coming from home the menu loader is finished when the orange loader start
+if ((document.referrer === 'https://dekube.be/') || (document.referrer === 'http://dekube.be/') || (document.referrer === 'http://localhost:3000/')){
+    console.log('coming from home');
+    // Start the page with the main loader opened (blocking the view)
     $('.page-loader-main').addClass('loaded');
+    $('.page-header').addClass('opened');
     setTimeout(function(){
         $('.page-loader-main').removeClass('loaded');
-        localStorage.setItem("isOpen", "False");
+        setTimeout(function(){
+            $('.page-header').removeClass('opened');
+        }, 50);
+        //was localStorage
+        sessionStorage.setItem("isOpen", "False");
     }, 50);
+}else if((sessionStorage.getItem("isOpen") == "True")){
+    // Start the page with the main loader opened (blocking the view)
+    $('.page-loader-main').addClass('loaded');
+    $('.page-header').addClass('opened');
+    setTimeout(function(){
+        $('.page-loader-main').removeClass('loaded');
+        setTimeout(function(){
+            $('.page-header').removeClass('opened');
+        }, 500);
+        //was localStorage
+        sessionStorage.setItem("isOpen", "False");
+    }, 50);
+}else{
+    $('.page-header').removeClass('opened');
+}
+
+// APPLY THE MENU LI CLASS WHEN VIEWING A WINDOW
+if ((sessionStorage.getItem("windowOpen") == "True")){
+    console.log("windowopened");
+    $('ul.menu li:first').addClass('newPageItem');
+    sessionStorage.setItem("windowOpen", "False");
 }
 
 $( document ).ready(function() {
@@ -44,7 +73,8 @@ $( document ).ready(function() {
     });
     // WHEN SWITCHING BETWEEN MENUS
     $('.pages-menu a').click(function(){
-        $('li.current-menu-item').removeClass('newPageItem');
+        $('#nav-icon2').removeClass('open');
+        $('li.newPageItem').removeClass('newPageItem');
         $('.link-home span.submenu').text('loading').addClass('loading');
         $('.page-loader-main').addClass('loading');
         sessionStorage.setItem("isOpen", "True");
@@ -54,7 +84,16 @@ $( document ).ready(function() {
         return false;
     });
     $('.window a.page-loader-scdn').click(function(){
+        sessionStorage.setItem("isOpen", "True");
+        $('li.newPageItem').removeClass('newPageItem');
+        $(".page-header").addClass('opened');
+        $('.page-loader-main').addClass('loading');
         $('.link-home span.submenu').text('loading').addClass('loading');
+        sessionStorage.setItem("windowOpen", "True");
+        var href = $(this).attr('href');
+        // Delay setting the location for one second
+        setTimeout(function() {window.location = href}, 600);
+        return false;
     });
     
 });
@@ -62,4 +101,5 @@ $( document ).ready(function() {
 $('.burger').click(function(){
     $(".page-header").toggleClass('opened');
     $('#nav-icon2').toggleClass('open');
+    $('body').toggleClass('burger-menu');
 });
